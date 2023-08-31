651e4f72275f57c0a19e297e1453d86bfc6dea43
Igor Raigorodskyi <raigorodskyi@gmail.com>
2023-08-30 22:02:38 -0400

    adding the modified files mavet

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,8 @@
 const darkModeButton = document.getElementById('dark-mode');
 const body = document.body;
+const wid1 = document.getElementById('widget1');
 
 darkModeButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
+    wid1.classList.toggle('dark-mode');
 });
