9af34839608d24f8b72de8f338309a4974d13460
Igor Raigorodskyi <raigorodskyi@gmail.com>
2023-09-06 22:34:00 -0400

    added the light turn on/off button

--- darkmode.js
+++ darkmode.js
@@ -6,3 +6,19 @@ darkModeButton.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
     wid1.classList.toggle('dark-mode');
 });
+
+
+// Add a click event listener to the button
+var button = document.getElementById('imageButton');
+button.addEventListener('click', changeImage);
+
+function changeImage() {
+    var img = document.getElementById('lightImage');
+    if (img.src.match("img/lightOff.png")) {
+        img.src = "img/lightOn.png";
+        img.alt = 'Light On';
+    } else {
+        img.src = "img/lightOff.png";
+        img.alt = 'Light Off';
+    }
+}
